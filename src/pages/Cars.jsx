f854594dbfc4a87bbb3ccc2b8899b297b7e5fcbc
Developer Phitany <developer@phitany.com>
2025-11-01 23:44:29 +0530

    updates

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -39,10 +39,13 @@ const matchesLocation =
         //  return matchesBrand && matchesPrice && matchesLocation;
 console.log(selectedseats)
       const matchesSeating =
-      !selectedseats ||
-      (car.seating_capacity &&
-        car.seating_capacity == selectedseats);
-         return matchesBrand && matchesPrice && matchesSeating && matchesLocation;
+    selectedseats === "All"
+      ? true
+      : Number(car.seating_capacity) === Number(selectedseats);
+console.log(selectedseats)
+      
+         
+      return matchesBrand && matchesPrice && matchesSeating && matchesLocation;
   });
 
   return (
@@ -91,7 +94,7 @@ console.log(selectedseats)
           onChange={(e) => setSeat(e.target.value)}
           className="border border-borderColor rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary"
         >
-          <option value="All" className='bg-white dark:bg-gray-400'>All</option>
+          <option value="" className='bg-white dark:bg-gray-400'>All</option>
           <option value="5" className='bg-white dark:bg-gray-400'>5 Seater</option>
           <option value="7" className='bg-white dark:bg-gray-400'>7 Seater</option>
         </select>
